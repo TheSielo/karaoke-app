d65397191835bc326335a902b4346f03536d47f7
Marcello Nobili <marcellonobili@pm.me>
2024-03-22 00:00:05 +0100

    - Implemented basic NewSongScreen ui.

--- app/src/main/java/com/sielotech/karaokeapp/activity/new_song/NewSongUI.kt
+++ app/src/main/java/com/sielotech/karaokeapp/activity/new_song/NewSongUI.kt
@@ -1,29 +1,33 @@
 package com.sielotech.karaokeapp.activity.new_song
 
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -53,32 +57,48 @@ internal object NewSongUI {
         ) { innerPadding ->
             val padding = remember { PaddingValues(all = 16.dp) }
             val textState = remember { mutableStateOf("") }
+            val step = remember { mutableIntStateOf(1) }
             Box(
                 Modifier
                     .padding(innerPadding)
                     .fillMaxHeight()
             ) {
                 Column(Modifier.padding(padding)) {
-                    Text(text = "Fill in the japanese lyrics:")
-                    HorizontalDivider(Modifier.height(16.dp))
-                    TextField(
-                        modifier = Modifier.fillMaxWidth(),
+                    OutlinedTextField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f),
                         value = textState.value,
-                        onValueChange = {
-                            newText -> textState.value = newText
-                        }
+                        onValueChange = { newText ->
+                            textState.value = newText
+                        },
+                        label = { Text(text = "Paste here the japanese lyrics") }
                     )
-                }
-                Row(
-                    Modifier
-                        .padding(padding)
-                        .align(Alignment.BottomCenter)
-                ) {
-                    Button(
-                        modifier = Modifier.fillMaxWidth(),
-                        onClick = { }
-                    ) {
-                        Text(text = "Next")
+                    Spacer(Modifier.height(16.dp))
+                    Row {
+                        Box(modifier = Modifier.weight(0.5f)) {
+                            androidx.compose.animation.AnimatedVisibility(
+                                visible = step.intValue > 1,
+                                enter = fadeIn(),
+                                exit = fadeOut()
+                            ) {
+                                OutlinedButton(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    onClick = { step.intValue-- }
+                                ) {
+                                    Text(text = "Previous")
+                                }
+                            }
+                        }
+
+                        Spacer(Modifier.width(16.dp))
+
+                        Button(
+                            modifier = Modifier.weight(0.5f),
+                            onClick = { step.intValue++ }
+                        ) {
+                            Text(text = "Next")
+                        }
                     }
                 }
             }
